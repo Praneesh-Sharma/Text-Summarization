6643083088798c22a41d69fb183477b768946b67
Praneesh Sharma <92104015+Praneesh-Sharma@users.noreply.github.com>
2023-11-10 15:49:12 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from transformers import pipeline
+
+# Load sentiment analysis pipeline
+summarizer = pipeline("summarization")
+
+# Streamlit UI
+st.title("Text Summarization App")
+text = st.text_area("Enter text:")
+if st.button("Analyze"):
+    if text:
+        summary = summarizer(article, max_length=150, do_sample=False)
+        gist = summary[0]['summary_text']
+        st.write(f"Summary: {gist}")
